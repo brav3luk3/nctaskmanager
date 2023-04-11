ad2c169f6c6a66b37539a2de69cb4f41d60f6724
brav3luk3 <oleksandrovich2000@gmail.com>
2023-04-11 20:42:11 +0300

    Task 4 refactoring

--- src/main/java/ua/edu/sumdu/j2se/semenyako/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/semenyako/tasks/AbstractTaskList.java
@@ -16,15 +16,20 @@ abstract public class AbstractTaskList {
 
     public abstract Task getTask(int index);
 
-    public AbstractTaskList incoming(int from, int to) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        AbstractTaskList tasksInAInterval = getClass().getConstructor().newInstance();
+    public AbstractTaskList incoming(int from, int to) {
+        AbstractTaskList tasksInAInterval;
+        try {
+            tasksInAInterval = getClass().getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e ) {
+            return null;
+        }
         for (int i = 0; i < size(); i++) {
-            if (!getTask(i).isActive()) {
-                continue;
-            }
-            if (to > getTask(i).nextTimeAfter(from) && getTask(i).nextTimeAfter(from) != -1) {
-                tasksInAInterval.add(getTask(i));
-            }
+                if (!getTask(i).isActive()) {
+                    continue;
+                }
+                if (to > getTask(i).nextTimeAfter(from) && getTask(i).nextTimeAfter(from) != -1) {
+                    tasksInAInterval.add(getTask(i));
+                }
         }
         return tasksInAInterval;
     }
